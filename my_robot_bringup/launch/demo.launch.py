be12ce54e9ce2a043ac5c47c2d091cfb737e2048
dhaboav <100108392+dhaboav@users.noreply.github.com>
2026-03-11 13:15:24 +0700

    feat: Add  launch package

---
+++ my_robot_bringup/launch/demo.launch.py
@@ -0,0 +1,48 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    turltesim_node = Node(
+        package="turtlesim", 
+        executable="turtlesim_node", 
+        name="turtle_node",
+    )
+
+    math_turtle_control_node = Node(
+        package="turtlesim_controller", 
+        executable="test_drive", 
+        name="controller_node",
+    )
+
+
+    ld.add_action(turltesim_node)
+    ld.add_action(math_turtle_control_node)
+
+    return ld
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # listener_node = Node(
+    #     package="demo_nodes_py", 
+    #     executable="listener", 
+    #     name="my_listener", 
+    #     remappings=[
+    #         ("chatter", "my_chatter") # Topics remappings
+    #     ]
+    # )
